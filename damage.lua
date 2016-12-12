9c6d24501da2912e5c0f8e689b8063db4644593a
Georg Eitler <funer@gmx.at>
2016-12-12 15:35:27 +0100

    initial commit

---
+++ damage.lua
@@ -0,0 +1,23 @@
+--// Damage Module //--
+
+StyleMeter.module.Damage = {
+	["priority"] = 1,
+	["strings"] = {
+		["SWING_DAMAGE"] = {
+			"arg12", --damage swing
+		--	"arg13", --overdamage swing
+		},
+		["RANGE_DAMAGE"] = {
+			"arg15", --damage range
+		--	"arg16", --overdamage range
+		},
+		["SPELL_DAMAGE"] = {
+			"arg15", --damage spell
+		--	"arg16", --overdamage spell
+		},
+		["SPELL_PERIODIC_DAMAGE"] = {
+			"arg15", --damage spell
+			--"arg16", --overdamage spell
+		},
+	},
+}
