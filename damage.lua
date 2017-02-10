47e51725d97fcce7b6f7151d336998450c2949a1
Georg Eitler <funer@gmx.at>
2017-02-10 13:57:02 +0100

    use a unified suffix
    also kick arguments for now

--- damage.lua
+++ damage.lua
@@ -3,22 +3,7 @@
 StyleMeter.module.Damage = {
 	["priority"] = 1,
 	["strings"] = {
-		["SWING_DAMAGE"] = {
-			"arg12", --damage swing
-		--	"arg13", --overdamage swing
-		},
-		["RANGE_DAMAGE"] = {
-			"arg15", --damage range
-		--	"arg16", --overdamage range
-		},
-		["SPELL_DAMAGE"] = {
-			"arg15", --damage spell
-		--	"arg16", --overdamage spell
-		},
-		["SPELL_PERIODIC_DAMAGE"] = {
-			"arg15", --damage spell
-			--"arg16", --overdamage spell
-		},
+		"_DAMAGE",
 	},
 }
 
